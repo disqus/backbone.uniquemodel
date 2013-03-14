1d6e8935fb35fbcf7ecf58c9d8f1a66af9f1a7d5
Ben Vinegar <ben@benv.ca>
2013-03-14 01:02:25 -0700

    Add modified Backbone TodoMVC example that uses UniqueModel

---
+++ todomvc/js/models/todo.js
@@ -0,0 +1,28 @@
+/*global Backbone */
+var app = app || {};
+
+(function () {
+	'use strict';
+
+	// Todo Model
+	// ----------
+
+	Backbone.UniqueModel.enableLocalStorage();
+
+	// Our basic **Todo** model has `title`, `order`, and `completed` attributes.
+	app.Todo = Backbone.UniqueModel(Backbone.Model.extend({
+		// Default attributes for the todo
+		// and ensure that each todo created has `title` and `completed` keys.
+		defaults: {
+			title: '',
+			completed: false
+		},
+
+		// Toggle the `completed` state of this todo item.
+		toggle: function () {
+			this.save({
+				completed: !this.get('completed')
+			});
+		}
+	}), 'Todo');
+})();
